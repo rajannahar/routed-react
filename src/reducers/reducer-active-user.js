7793208329541bac1b0781b996a60649c4e04c86
R_N101 <R_N101@msn.com>
2017-03-26 14:26:00 +0100

    add redux : adding comments to reducer! !

--- src/reducers/reducer-active-user.js
+++ src/reducers/reducer-active-user.js
@@ -7,3 +7,36 @@ export default function (state=null, action) {
 	}
 	return state;
 }
+
+
+
+// export default function (state=null, action) {
+// {/*reducer takes in state, action*/}
+
+// 	switch(action.type) {
+// 	/*switch statement based on action.type*/
+// 		case "USER_SELECTED": /*if action.type === "USER_SELECTED"*/
+// 			return action.payload; /*return the payload*/
+// 			break;
+// 		}
+
+// 		/*
+// 		case "ADD_PROJECT": {
+// 			const nextState = [
+// 				...currentState, 
+// 				{
+// 					id: 3, 
+// 					project: action.project, 
+// 					payload: action.payload
+// 				}
+// 			];
+// 			return nextState;
+// 			break;
+
+// 			default:
+// 				return: currentState
+// 		}
+// 		*/
+// 	}
+// 	return state;
+// }
